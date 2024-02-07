08a2652929519701130dbf32860a437f462ab0da
Dev Anand <ananddev681@gmail.com>
2024-02-08 01:03:53 +0530

    Lots of errors

---
+++ models/Branch.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const branchSchema = new Schema({
+    name: {
+        type: String,
+        enum : [
+            'Computer Science & Engineering',
+            'Electronics & Communication Engineering',
+            'Electrical Engineering',
+            'Mechanical Engineering',
+            'Civil Engineering',
+        ],
+        required: true
+    }
+});
+
+const Branch = mongoose.model('Branch', branchSchema);
+
+module.exports = Branch;
---
+++ models/QuestionPaper.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Branch = require('./Branch');
+
+const questionPaperSchema = new mongoose.Schema({
+    branch: {
+        type: Branch.schema,
+        required: true
+    },
+    semester: {
+        type: Number,
+        required: true,
+        min: 1, max: 8
+    },
+    year: {
+        type: Number,
+        required: true,
+        min: 1998, max: new Date().getFullYear()
+    },
+    subject: {
+        type: String,
+        required: true
+    },
+});
+
+const QuestionPaper = mongoose.model('QuestionPaper', questionPaperSchema);
+
+module.exports = QuestionPaper;
